1ccf5c9958544194e228a1b02ab727db7e5aea61
ndevu3399 <wachiramwas122@gmail.com>
2025-03-10 14:18:08 +0300

    modify index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression for divide
+const divide = function() {
+    return 2000 / 100;
+};
+
+// Arrow function for square
+const square = num => num * num;
+
+// Arrow function for add
+const add = (num1, num2) => num1 + num2;
+
+// Test output (optional, for debugging)
+console.log(divide()); // 20
+console.log(square(4)); // 16
+console.log(add(3, 7)); // 10
